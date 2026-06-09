0f302bccd516149b5a3bbdbdf882050e105d72a2
Angelo <garcia.1812424@studenti.uniroma1.it>
2026-06-09 19:06:24 +0200

    Rimosso codice commentato che mi ero scordato

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -193,6 +193,7 @@ router.patch('/updateFieldUser', async (req, res) => {
 router.get("/getAllUsersInfos", async (req, res) => {
   try {
     const usersInfos = await getAllUsersInfos();
+    //VA IN ERRORE LATO CLIENT PER COLPA DI isInBand CHE è NUMERO INVECE DI BOOLEANO
 
     if (!usersInfos || usersInfos.length === 0) {
       res.status(404).json({ 
@@ -218,28 +219,6 @@ router.get("/getAllUsersInfos", async (req, res) => {
     });
   }
 });
-
-/*router.get('/getAllUsersInfos', async (req, res) => {
-  try {
-    
-    // get all users infos
-    const users = await getAllUsersInfos();
-
-    if (!users || users.length === 0) {
-      return res.status(404).json({ error: 'No users found' }); // Nota: corretto anche l'inglese "Any user found" in "No users found"
-    }
-
-    console.log(`✅ Found users: ${users.length}`);
-
-    res.json({ 
-      message: 'Users retrieved successfully',
-      users: users
-    });
-
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to retrieve users', message: err.message });
-  }
-});*/
 
 /**
  * DELETE A USER
